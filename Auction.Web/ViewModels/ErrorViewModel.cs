bd6883cc674f4a1840b0e1721a0e0ea9affe8c98
senyich <arseniyneshetin@gmail.com>
2025-04-20 18:13:22 +0700

    upd

--- Auction.Web/ViewModels/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Auction.Web.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Auction.Web/ViewModels/ItemsViewModel.cs
+++
@@ -1,9 +0,0 @@
-using Auction.Domain.Models;
-
-namespace Auction.Web.ViewModels
-{
-    public class ItemsViewModel
-    {
-        public List<ItemModel> Items {get;set;}
-    }
-}
